28c1286bc935e4bde4cafb26461fa9e1e5b2a4aa
Lerato Mgwangqa <122816970+Ivyratermgwangqa@users.noreply.github.com>
2024-05-10 05:05:06 +0200

    Update 9-index_name_score.sql

--- 0x00-MySQL_Advanced/9-index_name_score.sql
+++ 0x00-MySQL_Advanced/9-index_name_score.sql
@@ -1,3 +1,3 @@
 -- Task 9: Create index on the first letter of name and the score
-CREATE INDEX idx_name_first_score
-ON names (SUBSTRING(name, 1, 1), score);
+-- Create an index on the first letter of 'name' and 'score' in the 'names' table
+CREATE INDEX idx_name_first_score ON names (LEFT(name, 1), score);
